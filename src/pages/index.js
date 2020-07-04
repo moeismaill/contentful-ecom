1312c600ea0118be3171c2cf98bdc36deba59a70
mrismail91 <mohammad.ismail9190@gmail.com>
2020-07-04 10:14:01 +0300

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 import Image from 'gatsby-image';
 import Layout from '../components/layout';
 import './index.css'
+import Hero from '../components/hero';
 
 export const query = graphql `
   {
@@ -41,6 +42,7 @@ const IndexPage = ({ data }) => {
   const products = data.allContentfulProduct.nodes;
   return (
     <Layout>
+      <Hero />
       <div className="products">
         {products.map(product => (
           <Link key={product.productSlug} to={`/products/${product.productSlug}`} style={{textDecoration: "none", color: "black"}}>
@@ -49,16 +51,6 @@ const IndexPage = ({ data }) => {
                   <h4>{product.productName}</h4>
                   <p className="price"> {product.price}</p>
                   <p className="price"> {product.discountedPrice}</p>
-                    <button
-                      className={`snipcart-add-item`}
-                      data-item-id={product.id}
-                      data-item-name={product.productName}
-                      data-item-image={product.mainimage.fluid.src}
-                      data-item-price={product.discountedPrice ? product.discountedPrice : product.price}
-                      data-item-url={`https://lucid-engelbart-e13bc0.netlify.app/${product.productSlug}`}
-                    >
-                      Add to Cart
-                    </button>
                 </div>
           </Link>
         ))}
